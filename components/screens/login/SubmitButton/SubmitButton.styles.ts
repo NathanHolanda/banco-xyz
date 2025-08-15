f9d50bd39bf878e1cf9237530bdd469054ab9d6f
NathanHolanda <nathan.holanda@revo360.io>
2025-08-15 19:13:17 -0300

    feat: creating submit button login component

---
+++ components/screens/login/SubmitButton/SubmitButton.styles.ts
@@ -0,0 +1,15 @@
+import { Palette } from "@/constants/Colors";
+import styled from "styled-components/native";
+
+export const StyledButton = styled.TouchableOpacity`
+  background-color: ${Palette.marineBlue};
+  border-radius: 20px;
+  padding: 15px;
+`;
+
+export const StyledButtonText = styled.Text`
+  color: ${Palette.white};
+  font-weight: bold;
+  font-size: 30px;
+  text-align: center;
+`;
---
+++ components/screens/login/SubmitButton/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { StyledButton, StyledButtonText } from "./SubmitButton.styles";
+
+export default function SubmitButton() {
+  return (
+    <StyledButton>
+      <StyledButtonText>Entrar</StyledButtonText>
+    </StyledButton>
+  );
+}
