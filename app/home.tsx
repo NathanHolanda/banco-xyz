186a6351e475740b083f0dd120275540482db93a
NathanHolanda <nathan.holanda@revo360.io>
2025-08-16 13:14:58 -0300

    feat: creating home interface

--- app/home.tsx
+++ app/home.tsx
@@ -1,10 +1,28 @@
+import ContentWrapper from "@/components/ContentWrapper";
+import BalanceCard from "@/components/screens/home/BalanceCard";
+import ItemCard from "@/components/screens/home/ItemCard";
+import ItemsContainer from "@/components/screens/home/ItemsContainer";
 import React from "react";
-import { Text, View } from "react-native";
+import { View } from "react-native";
 
 export default function Home() {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ContentWrapper>
+      <View style={{ gap: 20 }}>
+        <BalanceCard balance={1234.5} />
+        <ItemsContainer>
+          <ItemCard
+            iconName="paid"
+            title="Fazer uma transferência"
+            routeName="/makeTransfer"
+          />
+          <ItemCard
+            iconName="history"
+            title="Histórico de transferências"
+            routeName="/transfersHistory"
+          />
+        </ItemsContainer>
+      </View>
+    </ContentWrapper>
   );
 }
