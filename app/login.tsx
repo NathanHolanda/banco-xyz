0db4b0cca3dd6bd2472fa86260a48fc2595f7b2f
NathanHolanda <nathan.holanda@revo360.io>
2025-08-16 13:16:46 -0300

    feat: adding navigation from login to home

--- app/login.tsx
+++ app/login.tsx
@@ -1,17 +1,20 @@
 import ContentWrapper from "@/components/ContentWrapper";
 import Input from "@/components/screens/login/Input";
 import SubmitButton from "@/components/screens/login/SubmitButton";
+import { useRouter } from "expo-router";
 import React from "react";
 import { View } from "react-native";
 
 export default function Login() {
+  const router = useRouter();
+
   return (
     <ContentWrapper>
       <View style={{ marginBottom: 20 }}>
         <Input iconName="person" placeholder="E-mail" />
         <Input iconName="lock" placeholder="Senha" />
       </View>
-      <SubmitButton />
+      <SubmitButton onPress={() => router.replace("/home")} />
     </ContentWrapper>
   );
 }
