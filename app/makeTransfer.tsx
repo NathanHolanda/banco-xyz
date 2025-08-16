05f100c93f8cd9f2506a44c1e8bd6300077bec2d
NathanHolanda <nathan.holanda@revo360.io>
2025-08-16 13:00:41 -0300

    feat: creating makeTransfer and transfersHistory screen files

---
+++ app/makeTransfer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function makeTransfer() {
+  return (
+    <View>
+      <Text>makeTransfer</Text>
+    </View>
+  );
+}
---
+++ app/transfersHistory.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function transfersHistory() {
+  return (
+    <View>
+      <Text>transfersHistory</Text>
+    </View>
+  );
+}
