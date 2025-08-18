0fbe410a9f37ba222bb777b8e362d6de87d804f9
NathanHolanda <nathan.holanda@revo360.io>
2025-08-18 17:26:03 -0300

    refactor: removing fonts loading from makeTransfer _layout.tsx

--- app/makeTransfer/_layout.tsx
+++ app/makeTransfer/_layout.tsx
@@ -1,21 +1,9 @@
 import Header from "@/components/Header";
 import { Palette } from "@/utils/constants/Colors";
-import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import "react-native-reanimated";
 
 export default function RootLayout() {
-  const [loaded] = useFonts({
-    Roboto: require("../../assets/fonts/Roboto-Regular.ttf"),
-    "Roboto-Bold": require("../../assets/fonts/Roboto-Bold.ttf"),
-    "Roboto-SemiBold": require("../../assets/fonts/Roboto-SemiBold.ttf"),
-  });
-
-  if (!loaded) {
-    // Async font loading only occurs in development.
-    return null;
-  }
-
   return (
     <Stack
       screenOptions={{
