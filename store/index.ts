ffd52e5e2e1f86aa400ba82d39880933d60335f2
NathanHolanda <nathan.holanda@revo360.io>
2025-08-19 19:24:49 -0300

    feat: creating balanceSlice

--- store/index.ts
+++ store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
+import balanceSlice from "./balanceSlice";
 import userSlice from "./userSlice";
 
 export const store = configureStore({
   reducer: {
     user: userSlice.reducer,
+    balance: balanceSlice.reducer,
   },
 });
 
-export const actions = { ...userSlice.actions };
+export const actions = { ...userSlice.actions, ...balanceSlice.actions };
