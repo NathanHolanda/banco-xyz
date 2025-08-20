5e4acb0fb4baad577bc1e1237119d5d48782d96f
NathanHolanda <nathan.holanda@revo360.io>
2025-08-20 02:20:00 -0300

    feat: handling loading user error on loading screen

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,8 +2,9 @@ import Container from "@/components/screens/loading/Container";
 import { actions } from "@/store";
 import { Palette } from "@/utils/constants/Colors";
 import getFromAsyncStorage from "@/utils/functions/getFromAsyncStorage";
+import removeFromAsyncStorage from "@/utils/functions/removeFromAsyncStorage";
 import { useRouter } from "expo-router";
-import React, { useEffect } from "react";
+import React, { useCallback, useEffect } from "react";
 import { ActivityIndicator } from "react-native";
 import { useDispatch } from "react-redux";
 
@@ -11,16 +12,22 @@ export default function Loading() {
   const dispatch = useDispatch();
   const router = useRouter();
 
-  const checkLoggedUser = async () => {
-    const user = await getFromAsyncStorage("user");
-    if (!!user) {
-      dispatch(actions.setUser(user));
+  const checkLoggedUser = useCallback(async () => {
+    try {
+      const user = await getFromAsyncStorage("user");
+      if (!!user) {
+        dispatch(actions.setUser(user));
 
-      router.replace("/home");
-    } else {
+        router.replace("/home");
+      } else {
+        router.replace("/login");
+      }
+    } catch (err) {
+      removeFromAsyncStorage("user");
+
       router.replace("/login");
     }
-  };
+  }, [dispatch, router]);
 
   useEffect(() => {
     checkLoggedUser();
