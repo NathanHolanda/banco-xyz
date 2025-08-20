2a669d309cfde72fb8e7837581e0ea28ae9a141d
NathanHolanda <nathan.holanda@revo360.io>
2025-08-20 02:19:35 -0300

    feat: creating transfers history interface

--- app/transfersHistory.tsx
+++ app/transfersHistory.tsx
@@ -1,10 +1,107 @@
-import React from "react";
-import { Text, View } from "react-native";
+import ContentWrapper from "@/components/ContentWrapper";
+import EmptyListMessage from "@/components/EmptyListMessage";
+import HistoryFilters, {
+  HistoryFilter,
+} from "@/components/screens/transfersHistory/HistoryFilters";
+import TransferHistoryCard from "@/components/screens/transfersHistory/TransferHistoryCard";
+import ScreenTitle from "@/components/ScreenTitle";
+import { store } from "@/store";
+import { Palette } from "@/utils/constants/Colors";
+import cleanFormattedMoneyValue from "@/utils/functions/cleanFormattedMoneyValue";
+import Transfer from "@/utils/types/Transfer";
+import axios from "axios";
+import React, { useCallback, useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, View } from "react-native";
+import { Toast } from "toastify-react-native";
 
-export default function transfersHistory() {
+const initialFilter = {
+  value: "",
+  name: "",
+  date: "",
+};
+
+export default function TransfersHistory() {
+  const { user } = store.getState();
+
+  const [isFetching, setIsFetching] = useState(true);
+
+  const [transfers, setTransfers] = useState<Transfer[]>([]);
+  const [filteredTransfers, setFilteredTransfers] = useState<Transfer[]>([]);
+
+  const [historyFilter, setHistoryFilter] =
+    useState<HistoryFilter>(initialFilter);
+
+  const fetchTransfersList = useCallback(async () => {
+    try {
+      const {
+        data: { transfers },
+      } = await axios.get(`/api/transferlist/${user.id}`);
+
+      setTransfers(transfers);
+      setFilteredTransfers(transfers);
+    } catch (err) {
+      Toast.error("Desculpe, houve um erro ao carregar as transferências!");
+    } finally {
+      setIsFetching(false);
+    }
+  }, [user.id]);
+
+  const handleFilter = useCallback(() => {
+    setFilteredTransfers(
+      transfers.filter((transfer) => {
+        const hasFilteredName =
+          !historyFilter.name ||
+          transfer.payee.name
+            .toLocaleLowerCase()
+            .includes(historyFilter.name.toLocaleLowerCase());
+
+        const hasFilteredValue =
+          !historyFilter.value ||
+          transfer.value === cleanFormattedMoneyValue(historyFilter.value);
+
+        const hasFilteredDate =
+          !historyFilter.date || transfer.date === historyFilter.date;
+
+        return hasFilteredName && hasFilteredValue && hasFilteredDate;
+      })
+    );
+  }, [historyFilter.date, historyFilter.name, historyFilter.value, transfers]);
+
+  useEffect(() => {
+    fetchTransfersList();
+  }, []);
+
   return (
-    <View>
-      <Text>transfersHistory</Text>
-    </View>
+    <ContentWrapper>
+      <ScreenTitle iconName="history" text="Histórico de transferências" />
+      {isFetching ? (
+        <View style={{ flex: 1, justifyContent: "center" }}>
+          <ActivityIndicator size={60} color={Palette.darkBlue} />
+        </View>
+      ) : (
+        <View style={{ flex: 1 }}>
+          <HistoryFilters
+            handleFilter={handleFilter}
+            historyFilter={historyFilter}
+            setHistoryFilter={setHistoryFilter}
+          />
+          {!!filteredTransfers.length ? (
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={filteredTransfers}
+              renderItem={({ item }) => {
+                const itemAux: any = { ...item };
+                delete itemAux.userId;
+
+                return <TransferHistoryCard {...itemAux} />;
+              }}
+              keyExtractor={(_, index) => `transfer-${index}`}
+            />
+          ) : (
+            <EmptyListMessage text="Nenhuma transferência encontrada." />
+          )}
+        </View>
+      )}
+    </ContentWrapper>
   );
 }
