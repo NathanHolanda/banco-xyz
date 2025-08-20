8b67813b66f1d441a9dc536c7bfdbb6d01fbdf5d
NathanHolanda <nathan.holanda@revo360.io>
2025-08-20 02:10:21 -0300

    fix: uncomentting code on maskMoneyValue function

--- utils/functions/maskMoneyValue.ts
+++ utils/functions/maskMoneyValue.ts
@@ -1,9 +1,7 @@
 export default function maskMoneyValue(value: string) {
-  return (
-    value
-      .replace(/\D/g, "")
-      // .replace(/^000+/, "")
-      .replace(/(\d)(\d{2})$/, "$1,$2")
-      .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")
-  );
+  return value
+    .replace(/\D/g, "")
+    .replace(/^000+/, "")
+    .replace(/(\d)(\d{2})$/, "$1,$2")
+    .replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
 }
